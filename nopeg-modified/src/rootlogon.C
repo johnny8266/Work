3d62d22ab596249e9d20ff9d8e3b67d9521e6785
johnny8266 <johnny060615@gmail.com>
2021-01-08 18:12:00 +0100

    again

--- nopeg-modified/src/rootlogon.C
+++
@@ -1,7 +0,0 @@
-{
-  gSystem->Load("libTGenGeo.so");
-  gSystem->Load("libTGenBase.so");
-  gSystem->Load("libTGVKelly.so");
-  gSystem->Load("libTGenDVCS.so");
-  gSystem->Load("libTFDISTR.so");
-}
