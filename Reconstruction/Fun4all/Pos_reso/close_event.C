f33b82f6eddfc32054c883f44ed84ec347293141
johnny8266 <johnny060615@gmail.com>
2021-06-30 10:04:16 +0200

    Work with the closed event

--- Reconstruction/Fun4all/Pos_reso/close_event.C
+++ Reconstruction/Fun4all/Pos_reso/close_event.C
@@ -23,12 +23,14 @@ double E_shift_fit(double *x, double *par)
 void close_event()
 {
 
-  const int N_E_bin = 3;
-  string File_num[N_E_bin] = {"1.0", "5.0", "10.0"};
-  double e_per_bin[N_E_bin] = {1.0, 5.0, 10.0};
+  const int N_E_bin = 1;
+  // string File_num[N_E_bin] = {"1.0", "5.0", "10.0"};
+  // double e_per_bin[N_E_bin] = {1.0, 5.0, 10.0};
+  string File_num[N_E_bin] = {"10.0"};
+  double e_per_bin[N_E_bin] = {10.0};
   double e_statement[13] = {0.5, 1., 2., 3., 4., 5., 7., 10., 13., 18., 30., 50., 52.};
   
-  int flag = 0; // 0: glass, 1: crystal
+  int flag = 1; // 0: glass, 1: crystal
   TLorentzVector LV;
   double pri_Px = 0., pri_Py = 0., pri_Pz = 0.;
   double pjx = 0., pjy = 0., pjz = 0., diff_x = 0., diff_y = 0.;
@@ -38,23 +40,34 @@ void close_event()
   //
   TH1F *h1_e_diff[N_E_bin];
   TH1F *h1_posx_diff[N_E_bin], *h1_posy_diff[N_E_bin];
-  TH1F *recons_1[N_E_bin];
-  TH1F *recons_2[N_E_bin];
+  TH1F *recons_1_angle[N_E_bin];
+  TH1F *recons_2_angle[N_E_bin];
+  TH1F *recons_1_pos[N_E_bin];
+  TH1F *recons_2_pos[N_E_bin];
   TH1F *E_reco_1[N_E_bin];
   TH1F *E_reco_2[N_E_bin];
+  TH1F *zpos_reco_1[N_E_bin];
+  TH1F *zpos_reco_2[N_E_bin];
 
   TH2F *h2_posx_diff[N_E_bin], *h2_posy_diff[N_E_bin];
-  TH2F *cluster_1 = new TH2F("cluster_1", "cluster_1", 50, 30., 130., 50, 30., 130.);
-  TH2F *cluster_2 = new TH2F("cluster_2", "cluster_2", 50, 30., 130., 50, 30., 130.);
+  TH2F *cluster_1 = new TH2F("cluster_1", "cluster_1", 30, 0., 60., 30, 0., 60.);
+  TH2F *cluster_2 = new TH2F("cluster_2", "cluster_2", 30, 0., 60., 30, 0., 60.);
   TProfile *hprof_x[N_E_bin], *hprof_y[N_E_bin];
   
   for(int i = 0 ; i < N_E_bin ; i++)
     {
-      recons_1[i] = new TH1F(Form("recons_1_%d", i), Form("recons_1_%d", i), 50, 0., 2.);
-      recons_2[i] = new TH1F(Form("recons_2_%d", i), Form("recons_2_%d", i), 50, 0., 2.);
-
+      recons_1_angle[i] = new TH1F(Form("recons_1_angle_%d", i), Form("recons_1_angle_%d", i), 120, 0., 6.);
+      recons_1_angle[i]->SetLineColor(2);
+      recons_2_angle[i] = new TH1F(Form("recons_2_angle_%d", i), Form("recons_2_angle_%d", i), 120, 0., 6.);
+      recons_2_angle[i]->SetLineColor(4);
+      
       E_reco_1[i] = new TH1F(Form("E_reco_1_%d", i), Form("E_reco_1_%d", i), 100, 0., 20.);
       E_reco_2[i] = new TH1F(Form("E_reco_2_%d", i), Form("E_reco_2_%d", i), 100, 0., 20.);
+
+      zpos_reco_1[i] = new TH1F(Form("zpos_reco_1_%d", i), Form("zpos_reco_1_%d", i), 100, -200., -190.);
+      zpos_reco_1[i]->GetXaxis()->SetRangeUser(-200., -190.);
+      zpos_reco_2[i] = new TH1F(Form("zpos_reco_2_%d", i), Form("zpos_reco_2_%d", i), 100, -200., -190.);
+      zpos_reco_2[i]->GetXaxis()->SetRangeUser(-200., -190.);
       
       h1_posx_diff[i] = new TH1F(Form("h1_posx_diff_%d", i), Form("h1_posx_diff_%d", i), 120, -3., 3.);
       h1_posy_diff[i] = new TH1F(Form("h1_posy_diff_%d", i), Form("h1_posy_diff_%d", i), 120, -3., 3.);
@@ -77,10 +90,7 @@ void close_event()
       
       if( flag == 1 )
 	{
-	  if( i == 0)
-	    root_file_name = "/vol0/pwang-l/Singularity/my_det/sub_crystal/data/g4eemc_crystal_eval_mono_" + File_num[i] + "_GeV.root";
-	  else
-	    root_file_name = "/vol0/pwang-l/Singularity/my_det/sub_crystal/data/g4eemc_crystal_eval_mono_" + File_num[i] + ".0_GeV.root";
+	  root_file_name = "/vol0/pwang-l/Singularity/my_det/sub_crystal/data/pos/g4eemc_crystal_eval_mono_" + File_num[i] + "_GeV_close_event_study.root";
 	}
       else
 	{
@@ -106,12 +116,13 @@ void close_event()
       //
       TLorentzVector v_g, pr_1, pr_2;
       TVector3 p1, p2;
-      Float_t ge, gpt, geta, gphi, gvx, gvy, gvz;
+      Float_t ge, gpt, geta, gphi, gvx, gvy, gvz, gparticleID;
       Float_t event, clusterID, x, y, z, e, ntowers, e_base = 0., x_base = 0., y_base = 0., z_base = 0., n_tow_base = 0.;
       Float_t e_1_1, e_2_1, e_2_2;
       vector<float> x_reco, y_reco, z_reco, e_reco, N_towers, angle_2p;
+      vector<float> p1x, p1y, p1z, p2x, p2y, p2z;
       vector<int> i_th_cluster;
-      int count_single_clu = 0, count = 0;
+      int count_single_clu = 0, count_double_clu = 0, count = 0, ID_sum = 0;
 
 
 
@@ -129,20 +140,36 @@ void close_event()
 	  ntp_shower->GetEntry(j);
 	  pr_1.SetPtEtaPhiE(gpt, geta, gphi, ge);
 	  p1.SetXYZ(pr_1.Px(), pr_1.Py(), pr_1.Pz());
+	  p1x.push_back(pr_1.Px());  p1y.push_back(pr_1.Py());  p1z.push_back(pr_1.Pz());
+
 	  
 	  ntp_shower->GetEntry(j+1);
 	  pr_2.SetPtEtaPhiE(gpt, geta, gphi, ge);
 	  p2.SetXYZ(pr_2.Px(), pr_2.Py(), pr_2.Pz());
-		  
+	  p2x.push_back(pr_2.Px());  p2y.push_back(pr_2.Py());  p2z.push_back(pr_2.Pz());
 	  //	  cout << "Angle of 2 primary particle of event " << event << " th: " << p1.Angle(p2) / TMath::Pi() * 180. << endl;
 
 	  angle_2p.push_back(p1.Angle(p2) / TMath::Pi() * 180.);
+
+	  int j_eve = (int)event;
+
+	  if( j_eve == 1054 )
+	    {
+	      cluster_1->SetTitle(Form("p1[%.2f, %.2f, %.2f]      p2[%.2f, %.2f, %.2f]", pr_1.Px(), pr_1.Py(), pr_1.Pz(), pr_2.Px(), pr_2.Py(), pr_2.Pz()));	    
+	    }
+
+
+	  if( j_eve == 1053 )
+	    {
+	      cluster_2->SetTitle(Form("p1[%.2f, %.2f, %.2f]      p2[%.2f, %.2f, %.2f]", pr_1.Px(), pr_1.Py(), pr_1.Pz(), pr_2.Px(), pr_2.Py(), pr_2.Pz()));	    
+	    }
+	  
 	}
 
 
 
 
-      /*
+      
       ntp_tower->SetBranchAddress("event", &event);
       ntp_tower->SetBranchAddress("x", &x);
       ntp_tower->SetBranchAddress("y", &y);
@@ -155,14 +182,14 @@ void close_event()
 
 	  int j_eve = (int)event;
 
-	  if( j_eve == 9 )
+	  if( j_eve == 1054 )
 	    //	    cout << x << " " << y << " " << e << endl;
 	    cluster_1->Fill(x, y, e);
 
-	  if( j_eve == 21 )
+	  if( j_eve == 1053 )
 	    cluster_2->Fill(x, y, e);
 	}
-      */
+      
 
 
 
@@ -175,6 +202,7 @@ void close_event()
       ntp_cluster->SetBranchAddress("z", &z);
       ntp_cluster->SetBranchAddress("e", &e);
       ntp_cluster->SetBranchAddress("ntowers", &ntowers);
+      ntp_cluster->SetBranchAddress("gparticleID", &gparticleID);
 
       for(int j = 0 ; j < n_cluster ; j++)
 	{
@@ -203,25 +231,81 @@ void close_event()
 	      if( clusterID == 0 )
 		{
 		  //		  cout << "Only 1 cluster reconstructed: " << j_eve << endl;		  
-		  recons_1[i]->Fill(angle_2p[j_eve]);
+		  recons_1_angle[i]->Fill(angle_2p[j_eve]);
 		  E_reco_1[i]->Fill(e);
+		  zpos_reco_1[i]->Fill(z);
+		  count_single_clu++;
 		}
 
 	      if( clusterID == 1 )
 		{
 		  //		  cout << "2 clusters reconstructed: " << j_eve << endl;
-		  //		  recons_2[i]->Fill(angle_2p[j_eve]);
+		  //		  recons_2_angle[i]->Fill(angle_2p[j_eve]);
 		  e_2_1 = e;
 		  E_reco_2[i]->Fill(e_2_1);
+		  zpos_reco_2[i]->Fill(z);
+		  ID_sum = ID_sum + gparticleID;
+		  float D_1_1 = std::sqrt( std::pow((z / p1z[j_eve] * p1x[j_eve] - x), 2) + std::pow((z / p1z[j_eve] * p1y[j_eve] - y), 2) );
+		  float D_1_2 = std::sqrt( std::pow((z / p2z[j_eve] * p2x[j_eve] - x), 2) + std::pow((z / p2z[j_eve] * p2y[j_eve] - y), 2) );
+
+		  if( (D_1_1 > 10.) && (D_1_2 > 10.) )
+		    cout << j_eve << " " << z / p1z[j_eve] * p1x[j_eve] << " " << z / p1z[j_eve] * p1y[j_eve] << " || " << x << " " << y << endl;
+		  /*
+		  if( gparticleID < 1.5 )
+		    {
+		      if( D_1_1 > D_1_2 )
+			cout << "1 Wrong pair !!!  " << D_1_1 << " " << D_1_2 << " " << gparticleID << " " << angle_2p[j_eve] << endl;		      
+		    }
+		  else
+		    {
+		      if( D_1_1 < D_1_2 )
+			cout << "1 Wrong pair !!!" << endl;		      		      
+		    }
+		  */
+
+		  
 		  ntp_cluster->GetEntry(j - 2);
 		  e_2_2 = e;
 		  E_reco_2[i]->Fill(e_2_2);
-		  //		  E_reco_2[i]->Fill((e_2_1 + e_2_2));
-		  if( (e_2_1 > e_per_bin[i]) || (e_2_2 > e_per_bin[i]) )
+		  zpos_reco_2[i]->Fill(z);
+		  ID_sum = ID_sum + gparticleID;
+		  float D_2_1 = std::sqrt( std::pow((z / p1z[j_eve] * p1x[j_eve] - x), 2) + std::pow((z / p1z[j_eve] * p1y[j_eve] - y), 2) );
+		  float D_2_2 = std::sqrt( std::pow((z / p2z[j_eve] * p2x[j_eve] - x), 2) + std::pow((z / p2z[j_eve] * p2y[j_eve] - y), 2) );
+
+		  //		  if( (D_2_1 > 10.) && (D_2_2 > 10.) )
+		  if( (D_1_1 > 10.) && (D_1_2 > 10.) )
+		    cout << j_eve << " " << z / p1z[j_eve] * p1x[j_eve] << " " << z / p1z[j_eve] * p1y[j_eve] << " || " << x << " " << y << endl << endl;
+
+		  /*
+		  if( gparticleID < 1.5 )
 		    {
-		      cout << e_2_1 << " " << e_2_2 << endl;
-		      count_single_clu++;
-		      recons_2[i]->Fill(angle_2p[j_eve]);
+		      if( D_2_1 > D_2_2 )
+			cout << "2 Wrong pair !!!  " << D_2_1 << " " << D_2_2 << " " << gparticleID << " " << angle_2p[j_eve] << endl;		      
+		    }
+		  else
+		    {
+		      if( D_2_1 < D_2_2 )
+			cout << "2 Wrong pair !!!" << endl;		      		      
+		    }
+		  */
+
+		  
+		  // if( (ID_sum != 3) && (angle_2p[j_eve] > 1.5) )
+		  //   cout << e_2_1 << " " << e_2_2 << " " << ID_sum << " " << angle_2p[j_eve] << endl;
+
+		  ID_sum = 0;
+
+
+		  
+		  //		  E_reco_2[i]->Fill((e_2_1 + e_2_2));
+		  //		  if( (e_2_1 < e_per_bin[i]) && (e_2_2 < e_per_bin[i]) )
+		  if( (e_2_1 > e_per_bin[i]) || (e_2_2 > e_per_bin[i]) )		 
+		    {
+		      //		      cout << e_2_1 << " " << e_2_2 << endl;
+		      count_double_clu++;
+		      recons_2_angle[i]->Fill(angle_2p[j_eve]);
+
+		      
 		    }
 
 
@@ -238,50 +322,68 @@ void close_event()
 	      */
 	      
 	      count++;
-	      //	      count_single_clu++;
+	      //	      count_double_clu++;
 	      j = j - 1;
 
 	      e_base = 0.;  x_base = 0.;  y_base = 0.; z_base = 0.; n_tow_base = 0.;
 	    }
 	}
 
-      cout << endl << endl << "count: " << count_single_clu << endl << endl;
-      count_single_clu = 0;
+      cout << endl << endl << "Single cluster count: " << count_single_clu << endl;
+      cout << "Double cluster count: " << count_double_clu << endl << endl;
+      count_single_clu = 0;  count_double_clu = 0;
       
     }      
 
 
+
+
   
-  TCanvas *c1 = new TCanvas("c1", "c1", 1500, 1000);
-  c1->Divide(3,2);
+  /*
+  TCanvas *c1 = new TCanvas("c1", "c1", 1800, 600);
+  c1->Divide(3,1);
+  for(int i = 0 ; i < N_E_bin ; i++)
+    {
+      c1->cd( (i+1) );
+      
+      for(int j = 0 ; j < 2 ; j++)      
+	{
+	  //	  c1->cd( (i+1) + 3 * j );
+
+	  if(j == 0)
+	    recons_2_angle[i]->Draw();
+	  //	  else
+	    //	    recons_1_angle[i]->Draw("same");
+	}
+    }
+
+  
+  TCanvas *c2 = new TCanvas("c2", "c2", 1350, 900);
+  c2->Divide(3,2);
   for(int i = 0 ; i < N_E_bin ; i++)
     {
       for(int j = 0 ; j < 2 ; j++)      
 	{
-	  c1->cd( (i+1) + 3 * j );
+	  c2->cd( (i+1) + 3 * j );
 
 	  if(j == 0)
-	    {
-	      recons_1[i]->Draw();
-	      //	      E_reco_1[i]->Draw();
-	    }
+	    //	    zpos_reco_2[i]->Draw();
+	    E_reco_2[i]->Draw();
 	  else
-	    {
-	      recons_2[i]->Draw();
-	      //	      E_reco_2[i]->Draw();
-	    }
+	    //	    zpos_reco_1[i]->Draw();
+	    E_reco_1[i]->Draw();
 	}
     }
+  */
+
+
   
-
-
-  /*
-  TCanvas *c2 = new TCanvas("c2", "c2", 1000, 500);
+  TCanvas *c2 = new TCanvas("c2", "c2", 1600, 800);
   c2->Divide(2,1);
   c2->cd(1);
   cluster_1->Draw("colorz");
   c2->cd(2);
   cluster_2->Draw("colorz");
-  */
+  
   
 }
